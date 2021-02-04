c18fc2a101b906ca587e16f8e41aa4b4eba93043
Chathuranga Wijesinghe <chathuranga.w@eyepax.com>
2021-02-04 23:22:54 +0800

    init

---
+++ ElasticSearchIndexes/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ElasticSearchIndexes.Models;
+using Nest;
+using Elasticsearch.Net;
+
+namespace ElasticSearchIndexes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var uri = new Uri("http://localhost:9200");
+            var settings = new ConnectionConfiguration(uri);
+            var client = new ElasticClient(uri);
+            var searchResponse = await client.Cat.IndicesAsync();
+
+            if (searchResponse.IsValid)
+            {
+                return View(searchResponse.Records);
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ElasticSearchIndexes/Models/DashboardView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElasticSearchIndexes.Models
+{
+    public class DashboardView
+    {
+        public string ViewName { get; set; }
+        public string IndexName { get; set; }
+        public long ViewCount { get; set; }
+    }
+}
